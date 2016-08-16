3bfa4585aa5b3647215b054b713e56d684f10372
Jo Squire <mjsquire@digis.net>
2016-08-16 09:32:55 -0600

    cleaned merge

--- lib/russian_roulette.rb
+++ lib/russian_roulette.rb
@@ -1,6 +1,5 @@
 class RussianRoulette
   # attr_accessor :player, :wallet
-<<<<<<< HEAD
 
   def self.run_russian
     @bullets = [1, 2, 3, 4, 5, 6]
@@ -11,16 +10,4 @@ class RussianRoulette
       return "click"
     end
   end
-=======
-  def self.run_russian
-    @bullets = [1, 2, 3, 4, 5, 6]
-    @chamber = @bullets.sample
-    if @chamber == 6
-      return "bang"
-    else
-      return "click"
-    end
-  end
->>>>>>> jo
-
 end
